d1630e1538142386fdbd1e2b48d0b60270c14018
whoisrobinjha <robinjha370@gmail.com>
2023-08-10 16:47:30 +0530

    final linked list ready

--- Linked List/LInkedList.py
+++ Linked List/LInkedList.py
@@ -17,7 +17,33 @@ class LinkedList:
             current = current.next
         current.next = new_node
 
-#creating an empty linked list
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("END")
+
+    def delete(self, value):
+        if self.head is None:
+            return 
+        if self.head.data == value:
+            self.head = self.head.next
+            return
+        current = self.head
+        while current.next:
+            if current.next.data == value:
+                current.next = current.next.next
+                return
+            current = current.next
+
+# creating an empty linked list
 new_list = LinkedList()
+# adding to the linked list
 new_list.append(3)
 new_list.append(39)
+#Display Linked List
+new_list.display()
+#Delete from Linked List
+new_list.delete(3)
+new_list.display()
