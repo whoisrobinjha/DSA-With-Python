b8c547d320f0b023b86549db9a7685213153b8a2
whoisrobinjha <robinjha370@gmail.com>
2023-08-10 16:09:35 +0530

    file to create new linked list added

---
+++ Linked List/LInkedList.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = Node
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+#creating an empty linked list
+new_list = LinkedList()
