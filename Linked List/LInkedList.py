9d31d1fd436a4e5858a07d1dec6168727f3fe96b
whoisrobinjha <robinjha370@gmail.com>
2023-08-10 16:34:14 +0530

    Adding to linkedlist added

--- Linked List/LInkedList.py
+++ Linked List/LInkedList.py
@@ -1,11 +1,23 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self, data=None):
         self.data = data
-        self.next = Node
+        self.next = None
 
 class LinkedList:
     def __init__(self):
         self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
 
 #creating an empty linked list
 new_list = LinkedList()
+new_list.append(3)
+new_list.append(39)
